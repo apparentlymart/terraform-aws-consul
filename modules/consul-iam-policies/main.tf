ad3b8a84f8e907650452956002e14291e7a6bb5c
Martin Atkins <mart@degeneration.co.uk>
2019-05-09 17:47:53 -0700

    Upgrade to Terraform 0.12 syntax
    This is the result of running "terraform 0.12upgrade", along with some
    other manual updates that the upgrade tool was not able to make itself.

--- modules/consul-iam-policies/main.tf
+++ modules/consul-iam-policies/main.tf
@@ -3,10 +3,10 @@
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "aws_iam_role_policy" "auto_discover_cluster" {
-  count  = "${var.enabled}"
+  count  = var.enabled ? 1 : 0
   name   = "auto-discover-cluster"
-  role   = "${var.iam_role_id}"
-  policy = "${data.aws_iam_policy_document.auto_discover_cluster.json}"
+  role   = var.iam_role_id
+  policy = data.aws_iam_policy_document.auto_discover_cluster.json
 }
 
 data "aws_iam_policy_document" "auto_discover_cluster" {
@@ -22,3 +22,4 @@ data "aws_iam_policy_document" "auto_discover_cluster" {
     resources = ["*"]
   }
 }
+
